da438c000680915da7f26e60d0a3912395df5e4d
Alan Moura Santana <alan.moura2@hotmail.com>
2024-11-22 10:49:22 -0300

    associacao

---
+++ operador_associacao.py
@@ -0,0 +1,10 @@
+curso = "Curso de Python"
+frutas = ["laranja", "uva", "limão"] # Esse retorno ele é case sensetive
+saques = [1500, 100]
+
+print ("Python" in curso)
+
+print ("maça" in frutas)
+print ("laranja" not in frutas)
+
+# a Associação é para verificar se esta na mesma sequencia
