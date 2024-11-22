06302b7bfd03261f8dad695a95b2cdae33d051c0
Alan Moura Santana <alan.moura2@hotmail.com>
2024-11-22 09:22:07 -0300

    Operadores

---
+++ operadores_aritimeticos.py
@@ -0,0 +1,24 @@
+produto_1 = 20
+produto_2 = 10
+
+print (produto_1 + produto_2)
+print (produto_1 - produto_2)
+print (produto_1 / produto_2)
+print (produto_1 // produto_2)
+print (produto_1 * produto_2)
+print (produto_1 ** produto_2)  
+print (produto_1 % produto_2)     #  % é o resto da divisão
+
+x = (10 + 5) * 4
+y = (10 /2) + (25 * 2) - (2 ** 2) # ordem de execução é da esquerda para a direita
+
+
+print(x)
+print(y)
+
+## a ordem de é primeiro o 
+# parêntesis                ()
+# Expoênte                  **
+# Multiplicação e divisão   * /   (da esquerda para a direita)
+# Soma e Subtração          + -   (da esquerda para a direita)
+ 
